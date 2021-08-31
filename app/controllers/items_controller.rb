028285766f74c7a56be1357c48a32187ab38285c
Joe Christensen <joechristensenwork@gmail.com>
2021-08-31 10:54:21 -0700

    commit tree a958182bf813624242175112ad9000ebd5c1b9be

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_user_not_found_message
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(name: params[:name], description: params[:description], price: params[:price])
+    render json: item, status: :created
+  end
+
+  private
+  
+  def render_user_not_found_message
+    render json: { error: "User not found" }, status: :not_found
+  end
 end
